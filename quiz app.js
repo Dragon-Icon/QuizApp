4c47cef80c87d3d1897b517f2656457906244569
Adityaa <mulmiadityaa@gmail.com>
2021-04-04 13:13:12 +1300

    adi's best commit ever

---
+++ quiz app.js
@@ -0,0 +1,38 @@
+const quizData = [
+    {
+        question: "When is Adityaa Mulmi's birthday?",
+        a: 'Feb 29',
+        b: 'Mar 18',
+        c: 'Feb 9',
+        d: 'Feb 10',
+        correct: 'c'
+    },{
+        question: "What is the most used programming language in 2019?",
+        a: 'Java',
+        b:'C',
+        c:'Python',
+        d:'Javascript',
+        correct:'a'
+    },{
+        question: 'How old is Joe Biden?',
+        a: '69',
+        b: '82',
+        c: '75',
+        d: '78',
+        correct:'d'
+    },{
+        question: 'Who was the first president of Nepal?',
+        a: 'K.P Oli',
+        b: 'Matrika Prasad Koirala',
+        c: 'Gyanendra Bir Bikram Shah Dev',
+        d: 'Donald Trump',
+        correct: 'b'
+    },{
+        question:'What color is the of the airplane black box?',
+        a:'Black',
+        b:'Orange',
+        c:'Red',
+        d:'White',
+        correct:'b'
+    }
+]
